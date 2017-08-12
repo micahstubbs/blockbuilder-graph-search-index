490836631189b4837e3b8a7e8eeb824bfea6e9eb
Micah Stubbs <micah.stubbs@gmail.com>
2017-08-11 20:17:58 -0700

    update docker image name in stop-docker script

--- neo4j/docker/host/stop-docker.sh
+++ neo4j/docker/host/stop-docker.sh
@@ -1,2 +1,2 @@
 # Stop Docker containers of the dhimmel/hetionet image
-docker rm $(docker stop $(docker ps --all --quiet --filter ancestor=dhimmel/hetionet --format="{{.ID}}"))
+docker rm $(docker stop $(docker ps --all --quiet --filter ancestor=micahstubbs/blockbuilder-graph-search --format="{{.ID}}"))
