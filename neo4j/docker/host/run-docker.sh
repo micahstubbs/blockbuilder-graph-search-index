c694c1daa990e9a28461c66e2c2db4e0da1bfce5
Micah Stubbs <micah.stubbs@gmail.com>
2017-08-11 20:17:24 -0700

    update run-docker script

--- neo4j/docker/host/run-docker.sh
+++ neo4j/docker/host/run-docker.sh
@@ -2,14 +2,14 @@
 # See https://neo4j.com/developer/docker-3.x/ for doc
 docker run \
   --detach \
-  --name=hetionet-container \
+  --name=blockbuilder-graph-search-container \
   --restart=on-failure \
   --publish=80:7474 \
   --publish=443:7473 \
   --publish=7687:7687 \
-  --volume=$HOME/hetionet-data:/data \
+  --volume=$HOME/blockbuilder-graph-search-index/data:/data \
   --volume=$HOME/neo4j-logs:/logs \
   --volume=$HOME/ssl:/ssl \
   --env=NEO4J_dbms_memory_pagecache_size=512m \
   --env=NEO4J_dbms_memory_heap_maxSize=1g \
-  dhimmel/hetionet
+  micahstubbs/blockbuilder-graph-search
