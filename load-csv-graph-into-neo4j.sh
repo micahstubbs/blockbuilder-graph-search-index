a535227da88163da571d8b46962beed53e35aaf8
Micah Stubbs <micah.stubbs@gmail.com>
2017-08-18 22:24:14 -0700

    update load script to import user-built-block relationships

--- load-csv-graph-into-neo4j.sh
+++ load-csv-graph-into-neo4j.sh
@@ -4,8 +4,11 @@ GRAPH_DATABASE_DIR=data/databases/blockbuilder-graph-search.db
 CSV_DATA_DIR=/Users/m/workspace/blockbuilder-graph-search-index/data/csv-graphs-for-neo4j
 
 # build up paths to input csv files
+USERS=$CSV_DATA_DIR/users.csv
+USERS_BUILT_BLOCKS_RELATIONSHIPS=$CSV_DATA_DIR/user-built-block-relationships.csv
 README_LINKS_BLOCKS=$CSV_DATA_DIR/readme-links-blocks.csv
 README_LINKS_RELATIONSHIPS=$CSV_DATA_DIR/readme-links-relationships.csv
 
+
 cd $NEO4J_HOME
-./bin/neo4j-import --into $GRAPH_DATABASE_DIR --nodes $README_LINKS_BLOCKS --relationships $README_LINKS_RELATIONSHIPS
+./bin/neo4j-import --into $GRAPH_DATABASE_DIR --nodes $README_LINKS_BLOCKS --nodes $USERS --relationships $README_LINKS_RELATIONSHIPS --relationships $USERS_BUILT_BLOCKS_RELATIONSHIPS --multiline-fields=true
