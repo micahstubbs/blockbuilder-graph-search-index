535d98749a1868a522e34b0fd8fba42065e83a51
Micah Stubbs <micah.stubbs@gmail.com>
2017-08-19 12:21:11 -0700

    add script to parse users from multiple sources

--- load-csv-graph-into-neo4j.sh
+++ load-csv-graph-into-neo4j.sh
@@ -4,11 +4,18 @@ GRAPH_DATABASE_DIR=data/databases/blockbuilder-graph-search.db
 CSV_DATA_DIR=/Users/m/workspace/blockbuilder-graph-search-index/data/csv-graphs-for-neo4j
 
 # build up paths to input csv files
+
+# nodes source data
 USERS=$CSV_DATA_DIR/users.csv
+FUNCTIONS=$CSV_DATA_DIR/functions.csv
+BLOCKS=$CSV_DATA_DIR/blocks.csv
+
+# relationships source data
 USERS_BUILT_BLOCKS_RELATIONSHIPS=$CSV_DATA_DIR/user-built-block-relationships.csv
-README_LINKS_BLOCKS=$CSV_DATA_DIR/readme-links-blocks.csv
+BLOCK_CALLS_FUNCTION_RELATIONSHIPS=$CSV_DATA_DIR/block-calls-function-relationships.csv
 README_LINKS_RELATIONSHIPS=$CSV_DATA_DIR/readme-links-relationships.csv
 
 
+
 cd $NEO4J_HOME
-./bin/neo4j-import --into $GRAPH_DATABASE_DIR --nodes $README_LINKS_BLOCKS --nodes $USERS --relationships $README_LINKS_RELATIONSHIPS --relationships $USERS_BUILT_BLOCKS_RELATIONSHIPS --multiline-fields=true
+./bin/neo4j-import --into $GRAPH_DATABASE_DIR --nodes $BLOCKS --nodes $USERS --nodes $FUNCTIONS --relationships $README_LINKS_RELATIONSHIPS --relationships $USERS_BUILT_BLOCKS_RELATIONSHIPS --relationships $BLOCK_CALLS_FUNCTION_RELATIONSHIPS --multiline-fields=true
---
+++ parse-blocks.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const csvWriter = require('csv-write-stream');
+
+const inputDir = 'data/source-graphs';
+const outputDir = 'data/csv-graphs-for-neo4j';
+
+// load data synchronously for now
+// slower than async, but easier to reason about
+const readmeLinksInputFile = `${inputDir}/readme-blocks-graph.json`;
+const readmeLinksInputData = JSON.parse(
+  fs.readFileSync(readmeLinksInputFile, 'utf-8')
+);
+
+const functionsInputFile = `${inputDir}/blocks-api.json`;
+const functionsInputData = JSON.parse(
+  fs.readFileSync(functionsInputFile, 'utf-8')
+);
+
+const inputDatasets = [readmeLinksInputData, functionsInputData];
+const inputData = [].concat.apply([], inputDatasets);
+
+let outputData = [];
+const nodeHash = {};
+
+//
+// translate the node data from the existing format
+// to a format closer to what neo4j expects
+// following this guid https://neo4j.com/docs/operations-manual/3.2/tutorial/import-tool/
+//
+inputData.forEach(inputNode => {
+  const nodeObject = {};
+  // use neo4j conventions for keys
+  nodeObject['gistId:ID'] = inputNode.id;
+  // model users as nodes too later?
+  nodeObject.user = replaceNull(inputNode.user);
+  nodeObject.createdAt = replaceNull(inputNode.createdAt);
+  nodeObject.updatedAt = replaceNull(inputNode.updatedAt);
+  nodeObject.description = replaceNull(inputNode.description);
+  nodeObject[':LABEL'] = ''; // add tags here later
+  nodeHash[inputNode.id] = inputNode;
+});
+
+outputData = Object.keys(nodeHash).map(key => nodeHash[key]);
+
+// write a csv file
+let outputFile = `${outputDir}/blocks.csv`;
+let writer = csvWriter();
+writer.pipe(fs.createWriteStream(outputFile));
+outputData.forEach(d => {
+  writer.write(d);
+});
+writer.end();
+
+function replaceNull(value) {
+  if (value === null) {
+    return 'null';
+  } else if (typeof value === 'undefined') {
+    return 'undefined';
+  }
+  return value;
+}
