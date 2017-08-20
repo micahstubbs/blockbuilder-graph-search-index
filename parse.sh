ec595de5480ad636cd3af1e9d50e4837514cc1c6
Micah Stubbs <micah.stubbs@gmail.com>
2017-08-19 21:27:01 -0700

    add colors to parse.sh

--- parse.sh
+++ parse.sh
@@ -2,7 +2,9 @@
 node parse-blocks.js
 node parse-users.js
 node parse-functions.js
+node parse-colors.js
 
 # parse relationships
 node parse-user-built-block-relationships.js
 node parse-block-calls-function-relationships.js
+node parse-block-uses-color-relationships.js
