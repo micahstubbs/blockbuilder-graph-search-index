89a424e28b9a27c50834f7a8a06f57828006ca0f
Micah Stubbs <micah.stubbs@gmail.com>
2018-01-13 16:36:42 -0800

    add v2 setup script

---
+++ setup-server-v2.sh
@@ -0,0 +1,8 @@
+# a script to setup the new gcp server
+# perhaps a pattern for a container in the future
+
+
+# name
+# blockbuilder-graph-search-1
+# internal IP 10.138.0.2
+# external IP 35.203.147.195
