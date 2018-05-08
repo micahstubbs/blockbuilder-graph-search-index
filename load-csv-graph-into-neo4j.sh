c05d9f333718cbfefc3a89103309e0315614285e
@micahstubbs <micah.stubbs@gmail.com>
2018-05-07 18:06:14 -0700

    update script to load nodelist from readme-vis into neo4j as well

--- load-csv-graph-into-neo4j.sh
+++ load-csv-graph-into-neo4j.sh
@@ -1,5 +1,5 @@
 # store some paths as variable
-NEO4J_HOME=/Users/m/Documents/Neo4j
+# NEO4J_HOME=/Users/m/Documents/Neo4j
 GRAPH_DATABASE_DIR=data/databases/blockbuilder-graph-search.db
 CSV_DATA_DIR=/Users/m/workspace/blockbuilder-graph-search-index/data/csv-graphs-for-neo4j
 
@@ -9,7 +9,9 @@ CSV_DATA_DIR=/Users/m/workspace/blockbuilder-graph-search-index/data/csv-graphs-
 USERS=$CSV_DATA_DIR/users.csv
 FUNCTIONS=$CSV_DATA_DIR/functions.csv
 BLOCKS=$CSV_DATA_DIR/blocks.csv
+README_BLOCKS=$CSV_DATA_DIR/readme-links-blocks.csv
 COLORS=$CSV_DATA_DIR/colors.csv
+
 
 # relationships source data
 USERS_BUILT_BLOCKS_RELATIONSHIPS=$CSV_DATA_DIR/user-built-block-relationships.csv
@@ -19,4 +21,4 @@ BLOCK_USES_COLOR_RELATIONSHIPS=$CSV_DATA_DIR/block-uses-color-relationships.csv
 
 
 cd $NEO4J_HOME
-./bin/neo4j-import --into $GRAPH_DATABASE_DIR --nodes $BLOCKS --nodes $USERS --nodes $FUNCTIONS --nodes $COLORS --relationships $README_LINKS_RELATIONSHIPS --relationships $USERS_BUILT_BLOCKS_RELATIONSHIPS --relationships $BLOCK_CALLS_FUNCTION_RELATIONSHIPS --relationships $BLOCK_USES_COLOR_RELATIONSHIPS --multiline-fields=true
+./bin/neo4j-import --into $GRAPH_DATABASE_DIR --nodes $BLOCKS --nodes $README_BLOCKS --nodes $USERS --nodes $FUNCTIONS --nodes $COLORS --relationships $README_LINKS_RELATIONSHIPS --relationships $USERS_BUILT_BLOCKS_RELATIONSHIPS --relationships $BLOCK_CALLS_FUNCTION_RELATIONSHIPS --relationships $BLOCK_USES_COLOR_RELATIONSHIPS --multiline-fields=true
